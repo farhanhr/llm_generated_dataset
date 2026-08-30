4e3e9aa981168ef1d0f8f5fff5f6282737edd550
farhanhr <farhanhr00@gmail.com>
2026-08-30 21:18:35 +0700

    feat: classifier

--- main_classifier.py
+++ main_classifier.py
@@ -7,16 +7,19 @@ warnings.filterwarnings('ignore')
 from src.data_loader import SMSDataLoader
 from src.classifier import FakeNewsClassifier
 
-TIMESTAMP_RUN = "20260829201523"
+TIMESTAMP_RUN = "20260827182159"
 TARGET_FOLDER = f"data/augmented/{TIMESTAMP_RUN}/merged"
 RAW_DATA_PATH = "data/raw/sms_spam_indo.csv"
 
 def main():
     classifier = FakeNewsClassifier()
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    output_dir = f"results/classification/{timestamp}" 
+    
+    output_dir = f"results/classification/{TIMESTAMP_RUN}-classification"
+    if os.path.exists(output_dir):
+        time_suffix = datetime.now().strftime("%H%M%S")
+        output_dir = f"{output_dir}_{time_suffix}"
 
-    print("Melatih Model dengan data raw")
+    print("Training with raw data")
     loader = SMSDataLoader(RAW_DATA_PATH)
     normal_df, spam_df = loader.process()
     baseline_df = pd.concat([normal_df, spam_df], ignore_index=True)
@@ -25,12 +28,11 @@ def main():
     classifier.save_classification_results(baseline_metrics, "Baseline_Data_Asli", output_dir)
     print(f"-> Selesai! (Akurasi: {baseline_metrics['Akurasi']:.4f} | F1: {baseline_metrics['F1-Score']:.4f})")
 
-    print(f"\nMelatih model dengan Data Augmentasi LLM {TARGET_FOLDER}...")
+    print(f"\nTraining for: {TARGET_FOLDER}...")
     for file_name in os.listdir(TARGET_FOLDER):
         if file_name.endswith('.csv'):
             file_path = os.path.join(TARGET_FOLDER, file_name)
             df_merged = pd.read_csv(file_path)
-
             df_merged = df_merged.dropna(subset=['Pesan', 'Kategori'])
 
             print(f"-> Melatih XGBoost pada: {file_name}")
@@ -39,7 +41,7 @@ def main():
             dataset_name = file_name.replace('.csv', '')
             classifier.save_classification_results(metrics, dataset_name, output_dir)
 
-    print(f"\nSelesai! Hasil klasifikasi tersimpan di: {output_dir}")
+    print(f"\nResult saved at: {output_dir}")
 
 if __name__ == "__main__":
     main()
