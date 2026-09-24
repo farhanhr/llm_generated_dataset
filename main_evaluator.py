82d178a2e1a505b164a6dcec6917477e8f61e90a
farhanhr <farhanhr00@gmail.com>
2026-09-25 04:13:06 +0700

    feat: notebook v2

--- main_evaluator.py
+++ main_evaluator.py
@@ -6,7 +6,7 @@ warnings.filterwarnings('ignore')
 
 from src.evaluator import TextEvaluator
 
-TIMESTAMP_RUN = "20260905193857" 
+TIMESTAMP_RUN = "20260913182453" 
 TARGET_LOG_FOLDER = f"data/augmented/{TIMESTAMP_RUN}/augmented_log"
 
 def main():
